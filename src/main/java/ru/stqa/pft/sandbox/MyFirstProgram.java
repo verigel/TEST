01ef9ecfe07358259ab2a010b9a1247eaf58afa9
Илья Скворцов <ilyaskvortsov1994@gmail.com>
2020-04-29 01:07:00 +0300

    sdfsdf

--- src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,5 +5,18 @@ public class MyFirstProgram {
 
  public static void main(String[] args) {
    System.out.println("Hello, world");
-  }
+
+     double l = 5;
+     System.out.println("Площадь квадрата со стороной " + l + "="+ area(l));
+     double a = 4;
+     double b = 6;
+     System.out.println("Площадь прямоугольника со сторонами " + a +" и " + b  + "="+ area(l));
+ }
+
+    public static double  area(double len) {
+        return  len * len;
+    }
+    public static double area(double a, double b) {
+        return a * b;
+    }
 }
